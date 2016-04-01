56bdb202eaa9bf9a88e98ba84a31676af8463b88
Rafael Diego Nicoletti <rafael.nicoletti@novitech.com.br>
2016-04-01 12:44:18 -0300

    add unit test to test mute value conversion

---
+++ SipStack.Isup.Testing/Alaw/TestConvertMuteData.cs
@@ -0,0 +1,34 @@
+﻿namespace SipStack.Tests.Alaw
+{
+
+    using NUnit.Framework;
+
+    using TestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
+
+    /// <summary>
+    /// Summary description for TestConvertMuteData
+    /// </summary>
+    [TestFixture]
+    public class TestEncoding
+    {
+        [Test]
+        public void TestMuteValue()
+        {
+            var mutePCM = new byte[] { 0x0, 0x0 };
+
+            var expectedAlaw = 0xd5;
+
+            Assert.AreEqual(expectedAlaw, AlawMediaCodec.AlawEncoder.LinearToAlaw(mutePCM[0], mutePCM[1]));
+        }
+
+        [Test]
+        public void TestAnotherValue()
+        {
+            var mutePCM = new byte[] { 0x0, 0x0 };
+
+            var expectedAlaw = 0xd5;
+
+            Assert.AreEqual(expectedAlaw, AlawMediaCodec.AlawEncoder.LinearToAlaw(mutePCM[0], mutePCM[1]));
+        }
+    }
+}
